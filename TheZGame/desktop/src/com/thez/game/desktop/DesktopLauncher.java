49438e4be87ab7261499e4653f13b1a1a8888f23
raiky19 <ryankoym@gmail.com>
2018-01-12 16:57:15 -0600

    Setup project

---
+++ TheZGame/desktop/src/com/thez/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.thez.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.thez.game.TheZGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TheZGame(), config);
+	}
+}
