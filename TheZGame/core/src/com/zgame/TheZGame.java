d6fe8fc0a5fd7d40744f8b6685e3283c37240e71
Brandon Rodgers <brandonx87@gmail.com>
2018-04-17 00:37:35 -0500

    Added a few lines to test out adding a string to the game and to test out using git to commit and push the changes.

--- TheZGame/core/src/com/zgame/TheZGame.java
+++ TheZGame/core/src/com/zgame/TheZGame.java
@@ -6,6 +6,10 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.zgame.ui.InputManager;
 import com.zgame.world.EcsManager;
+import com.badlogic.gdx.graphics.Color; //bjr
+import com.badlogic.gdx.graphics.g2d.SpriteBatch; //bjr
+import com.badlogic.gdx.graphics.g2d.BitmapFont; //bjr
+
 
 public class TheZGame extends ApplicationAdapter {
 	
@@ -13,6 +17,9 @@ public class TheZGame extends ApplicationAdapter {
 	InputManager gameInputManager;
 	InputManager uiInputManager;
 	EcsManager ecsManager;
+	
+	private SpriteBatch batch; //bjr
+	private BitmapFont font; //bjr
 	
 	@Override
 	public void create () {
@@ -30,15 +37,26 @@ public class TheZGame extends ApplicationAdapter {
 		ecsManager.createZombie(120, 220);
 		ecsManager.createZombie(300, 100);
 		
+		batch = new SpriteBatch(); //bjr
+		font = new BitmapFont(); //bjr
+		font.setColor(Color.RED); //bjr
+		font.getData().setScale(2); //bjr
+		
 		//Create Systems
 	}
 
 	@Override
 	public void render () {
 		ecsManager.update();
+		
+		batch.begin(); //bjr
+		font.draw(batch, "Hey Ryan!", 200, 250); //bjr
+		batch.end(); //bjr
 	}
 	
 	@Override
 	public void dispose () {
+		batch.dispose(); //bjr
+		font.dispose(); //bjr
 	}
 }
--- TheZGame/desktop/src/com/thez/game/desktop/DesktopLauncher.java
+++ TheZGame/desktop/src/com/thez/game/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.zgame.TheZGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "The Z Game";
 		new LwjglApplication(new TheZGame(), config);
 	}
 }
